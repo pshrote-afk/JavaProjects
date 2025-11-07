8071b242492bb9e214f83e1ee786e010cebcfbee
pshrote-afk <pshrote1@gmail.com>
2025-11-07 10:31:11 +0530

    #54 format pl folder's .java files to Google Java Style

--- hr/pl/src/main/java/com/thinking/machines/hr/pl/Main.java
+++ hr/pl/src/main/java/com/thinking/machines/hr/pl/Main.java
@@ -1,11 +1,11 @@
 package com.thinking.machines.hr.pl;
-import com.thinking.machines.hr.pl.ui.*;
+
 import com.thinking.machines.hr.pl.model.*;
-public class Main
-{
-public static void main(String args[])
-{
-CourseUI courseUI = new CourseUI();
-courseUI.setVisible(true);
-}
+import com.thinking.machines.hr.pl.ui.*;
+
+public class Main {
+  public static void main(String args[]) {
+    CourseUI courseUI = new CourseUI();
+    courseUI.setVisible(true);
+  }
 }
